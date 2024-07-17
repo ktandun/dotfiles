0cb75978fd705102557e3eba2bafa94751627252
Kenzie Tandun <nudnateiznek@gmail.com>
2024-07-17 13:17:59 +1200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -226,6 +226,15 @@ require('lazy').setup({
                             }
                         end
                     },
+                    htmldjango = {
+                        function()
+                            return {
+                                exe = 'djhtml',
+                                args = { '-', '--reformat' },
+                                stdin = true
+                            }
+                        end
+                    },
                     ['*'] = {
                         require('formatter.filetypes.any').remove_trailing_whitespace
                     }
@@ -383,6 +392,59 @@ require('lazy').setup({
                 indent = { enable = true }
             })
         end
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        dependencies = 'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                textobjects = {
+                    select = {
+                        enable = true,
+
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
+
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            -- nvim_buf_set_keymap) which plugins like which-key display
+                            ['ic'] = {
+                                query = '@class.inner',
+                                desc = 'Select inner part of a class region'
+                            },
+                        },
+                        -- You can choose the select mode (default is charwise 'v')
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * method: eg 'v' or 'o'
+                        -- and should return the mode ('v', 'V', or '<c-v>') or a table
+                        -- mapping query_strings to modes.
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V', -- linewise
+                            ['@class.outer'] = '<c-v>' -- blockwise
+                        },
+                        -- If you set this to `true` (default is `false`) then any textobject is
+                        -- extended to include preceding or succeeding whitespace. Succeeding
+                        -- whitespace has priority in order to act similarly to eg the built-in
+                        -- `ap`.
+                        --
+                        -- Can also be a function which gets passed a table with the keys
+                        -- * query_string: eg '@function.inner'
+                        -- * selection_mode: eg 'v'
+                        -- and should return true or false
+                        include_surrounding_whitespace = true
+                    }
+                }
+            }
+
+        end
+
     }
 }, {
     performance = { rtp = { disabled_plugins = { 'matchparen' } } },
@@ -459,7 +521,10 @@ require'lspconfig'.lua_ls.setup {
     settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
 }
 require'lspconfig'.tsserver.setup { capabilities = capabilities }
-require'lspconfig'.basedpyright.setup { capabilities = capabilities }
+require'lspconfig'.basedpyright.setup {
+    capabilities = capabilities,
+    settings = { basedpyright = { analysis = { typeCheckingMode = 'basic' } } }
+}
 require'lspconfig'.csharp_ls.setup {
     -- specify root_dir, so lsp can find all solutions related to your workspace
     capabilities = capabilities,
