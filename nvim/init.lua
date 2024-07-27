4b8eed9ce83d5a74d8b7b6f0936a6e86171fdd48
Kenzie Tandun <nudnateiznek@gmail.com>
2024-07-27 17:44:30 +1200

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,11 @@
+local home = vim.fn.expand('$HOME')
+
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
     vim.fn.system({
-        'git',
-        'clone',
-        '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git',
-        '--branch=stable', -- latest stable release
+        'git', 'clone', '--filter=blob:none',
+        'https://github.com/folke/lazy.nvim.git', '--branch=stable', -- latest stable release
         lazypath
     })
 end
@@ -25,8 +24,7 @@ require('lazy').setup({
             vim.g.gruvbox_material_enable_italic = true
             vim.cmd.colorscheme('gruvbox-material')
         end
-    },
-    {
+    }, {
         'echasnovski/mini.nvim',
         version = '*',
         config = function()
@@ -53,7 +51,7 @@ require('lazy').setup({
             })
             require('mini.pairs').setup({
                 -- In which modes mappings from this `config` should be created
-                modes = { insert = true, command = false, terminal = false },
+                modes = {insert = true, command = false, terminal = false},
 
                 -- Global mappings. Each right hand side should be a pair information, a
                 -- table with at least these fields (see more in |MiniPairs.map|):
@@ -99,26 +97,24 @@ require('lazy').setup({
                         action = 'closeopen',
                         pair = '""',
                         neigh_pattern = '[^\\].',
-                        register = { cr = false }
+                        register = {cr = false}
                     },
                     ['\''] = {
                         action = 'closeopen',
                         pair = '\'\'',
                         neigh_pattern = '[^%a\\].',
-                        register = { cr = false }
+                        register = {cr = false}
                     },
                     ['`'] = {
                         action = 'closeopen',
                         pair = '``',
                         neigh_pattern = '[^\\].',
-                        register = { cr = false }
+                        register = {cr = false}
                     }
                 }
             })
         end
-    },
-    { 'vimpostor/vim-lumen' },
-    {
+    }, {'vimpostor/vim-lumen'}, {
         'NeogitOrg/neogit',
         dependencies = {
             'nvim-lua/plenary.nvim', -- required
@@ -126,102 +122,25 @@ require('lazy').setup({
             'nvim-telescope/telescope.nvim' -- optional
         },
         config = true
-    },
-    {
+    }, {
         'vhyrro/luarocks.nvim',
         priority = 1000,
         config = true,
-        opts = { rocks = { 'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua' } }
-    },
-    {
+        opts = {rocks = {'lua-curl', 'nvim-nio', 'mimetypes', 'xml2lua'}}
+    }, {
         'stevearc/oil.nvim',
-        keys = { { '<leader>o', '<cmd>Oil<cr>', desc = 'Oil' } },
+        keys = {{'<leader>o', '<cmd>Oil<cr>', desc = 'Oil'}},
         opts = {},
         -- Optional dependencies
-        dependencies = { 'nvim-tree/nvim-web-devicons' }
-    },
-    { 'neovim/nvim-lspconfig' },
-    {
-        'mhartington/formatter.nvim',
+        dependencies = {'nvim-tree/nvim-web-devicons'}
+    }, {'neovim/nvim-lspconfig'}, {
+        'sbdchd/neoformat',
         config = function()
-            local util = require 'formatter.util'
-            require('formatter').setup {
-                logging = true,
-                log_level = vim.log.levels.WARN,
-                filetype = {
-                    typescriptreact = {
-                        function()
-                            return {
-                                exe = 'biome',
-                                args = {
-                                    'format',
-                                    '--stdin-file-path=' ..
-                                        util.escape_path(
-                                            util.get_current_buffer_file_path())
-                                },
-                                stdin = true
-                            }
-                        end
-                    },
-                    lua = {
-                        function()
-                            return {
-                                exe = 'lua-format',
-                                args = {
-                                    '--spaces-inside-table-braces',
-                                    '--double-quote-to-single-quote',
-                                    '--break-after-operator',
-                                    '--no-keep-simple-control-block-one-line',
-                                    '--no-keep-simple-function-one-line',
-                                    '--chop-down-table',
-                                    '--chop-down-kv-table',
-                                    '--chop-down-parameter',
-                                    util.escape_path(
-                                        util.get_current_buffer_file_path())
-                                },
-                                stdin = true
-                            }
-                        end
-                    },
-                    cs = {
-                        function()
-                            return {
-                                exe = 'dotnet-csharpier',
-                                args = {
-                                    '--write-stdout',
-                                    util.escape_path(
-                                        util.get_current_buffer_file_path())
-                                },
-                                stdin = true
-                            }
-                        end
-                    },
-                    python = {
-                        function()
-                            return {
-                                exe = 'black',
-                                args = { '-q', '-' },
-                                stdin = true
-                            }
-                        end
-                    },
-                    htmldjango = {
-                        function()
-                            return {
-                                exe = 'djhtml',
-                                args = { '-', '--reformat' },
-                                stdin = true
-                            }
-                        end
-                    },
-                    ['*'] = {
-                        require('formatter.filetypes.any').remove_trailing_whitespace
-                    }
-                }
-            }
+            vim.cmd [[
+				let g:neoformat_try_node_exe = 1 " try to find exe on node_modules
+			]]
         end
-    },
-    {
+    }, {
         'williamboman/mason.nvim',
         config = function()
             local mason = require 'mason'
@@ -235,29 +154,19 @@ require('lazy').setup({
                 }
             })
         end
-    },
-    {
+    }, {
         'williamboman/mason-lspconfig.nvim',
         config = function()
             local mason = require 'mason-lspconfig'
             mason.setup({
                 ensure_installed = {
-                    'bashls',
-                    'lua_ls',
-                    'basedpyright',
-                    'csharp_ls',
-                    'tsserver'
+                    'bashls', 'lua_ls', 'biome', 'tailwindcss', 'basedpyright',
+                    'csharp_ls', 'tsserver', 'volar'
                 }
             })
         end
-    },
-    { 'hrsh7th/cmp-nvim-lsp' },
-    { 'hrsh7th/cmp-buffer' },
-    { 'hrsh7th/cmp-path' },
-    { 'hrsh7th/cmp-cmdline' },
-    { 'hrsh7th/cmp-vsnip' },
-    { 'hrsh7th/vim-vsnip' },
-    {
+    }, {'hrsh7th/cmp-nvim-lsp'}, {'hrsh7th/cmp-buffer'}, {'hrsh7th/cmp-path'},
+    {'hrsh7th/cmp-cmdline'}, {'hrsh7th/cmp-vsnip'}, {'hrsh7th/vim-vsnip'}, {
         'hrsh7th/nvim-cmp',
         config = function()
             local cmp = require('cmp')
@@ -268,25 +177,23 @@ require('lazy').setup({
                     end
                 },
                 sources = cmp.config.sources({
-                    { name = 'nvim_lsp' },
-                    { name = 'vsnip' } -- For vsnip users.
-                }, { { name = 'buffer' } }),
+                    {name = 'nvim_lsp'}, {name = 'vsnip'} -- For vsnip users.
+                }, {{name = 'buffer'}}),
                 mapping = cmp.mapping.preset.insert({
-                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(), -- trigger auto complete
+                    ['<CR>'] = cmp.mapping.confirm({select = true}),
                     ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(),
-                                            { 'i', 'c' }),
+                                            {'i', 'c'}),
                     ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(),
-                                            { 'i', 'c' })
+                                            {'i', 'c'})
                 })
             }
         end
-    },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-    {
+    }, {'nvim-telescope/telescope-fzf-native.nvim', build = 'make'}, {
         'nvim-telescope/telescope.nvim',
         tag = '0.1.6',
         -- or                              , branch = '0.1.x',
-        dependencies = { 'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep' },
+        dependencies = {'nvim-lua/plenary.nvim', 'BurntSushi/ripgrep'},
         config = function()
             -- telescope
             local telescope = require('telescope')
@@ -294,7 +201,7 @@ require('lazy').setup({
                 defaults = {
                     layout_strategy = 'vertical',
                     layout_config = {
-                        vertical = { width = 0.8 }
+                        vertical = {width = 0.8}
                         -- other layout configuration here
                     }
                     -- other defaults configuration here
@@ -314,17 +221,18 @@ require('lazy').setup({
 
             local builtin = require('telescope.builtin')
 
-            vim.keymap.set('n', '<leader>f', function()
-                builtin.find_files({ hidden = true })
+            vim.keymap.set('n', '<leader>f',
+                           function()
+                builtin.find_files({hidden = true})
             end, {})
             vim.keymap.set('n', '<leader>b', builtin.buffers, {})
             vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
             vim.keymap.set('n', 'gR', builtin.lsp_references, {})
 
-            local default_livegrep_args = { '--hidden' }
+            local default_livegrep_args = {'--hidden'}
 
             vim.keymap.set('n', '<leader>g', function()
-                builtin.live_grep({ additional_args = default_livegrep_args })
+                builtin.live_grep({additional_args = default_livegrep_args})
             end, {})
             vim.keymap.set('n', '<leader>4', function()
                 local word = vim.fn.expand('<cword>')
@@ -341,23 +249,20 @@ require('lazy').setup({
                 })
             end, {})
         end
-    },
-    {
+    }, {
         'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        dependencies = {'nvim-tree/nvim-web-devicons'},
         config = function()
             local lualine = require 'lualine'
             lualine.setup({
-                sections = { lualine_a = {}, lualine_c = { 'filename' } }
+                sections = {lualine_a = {}, lualine_c = {'filename'}}
             })
         end
-    },
-    {
+    }, {
         'chrisgrieser/nvim-scissors',
         dependencies = 'nvim-telescope/telescope.nvim', -- optional
-        opts = { snippetDir = '~/.config/nvim/snippets' }
-    },
-    {
+        opts = {snippetDir = '~/.config/nvim/snippets'}
+    }, {
         'nvim-treesitter/nvim-treesitter',
         build = ':TSUpdate',
         config = function()
@@ -367,12 +272,11 @@ require('lazy').setup({
                 ensure_installed = 'all',
                 sync_install = false,
                 auto_install = false,
-                highlight = { enable = true },
-                indent = { enable = true }
+                highlight = {enable = true},
+                indent = {enable = true}
             })
         end
-    },
-    {
+    }, {
         'nvim-treesitter/nvim-treesitter-textobjects',
         dependencies = 'nvim-treesitter/nvim-treesitter',
         config = function()
@@ -394,7 +298,7 @@ require('lazy').setup({
                             ['ic'] = {
                                 query = '@class.inner',
                                 desc = 'Select inner part of a class region'
-                            },
+                            }
                         },
                         -- You can choose the select mode (default is charwise 'v')
                         --
@@ -426,8 +330,8 @@ require('lazy').setup({
 
     }
 }, {
-    performance = { rtp = { disabled_plugins = { 'matchparen' } } },
-    checker = { enabled = false, notify = true },
+    performance = {rtp = {disabled_plugins = {'matchparen'}}},
+    checker = {enabled = false, notify = true},
     change_detection = {
         -- automatically check for config file changes and reload the ui
         enabled = true,
@@ -458,6 +362,14 @@ vim.cmd [[
   smap <expr> <C-k> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<C-k>'
 ]]
 
+-- Format .vue files on save
+vim.api.nvim_exec([[
+  augroup fmt
+	autocmd!
+	autocmd BufWritePre *.vue,*.ts undojoin | Neoformat
+  augroup END
+]], false)
+
 local function get_directory_path_with_dots(path)
     -- Define a pattern to match the directory path
     local pattern = '(.*)/[^/]+$'
@@ -478,13 +390,11 @@ end
 vim.keymap.set('n', '<leader>ns', '<cmd>lua WriteNamespace()<CR>')
 
 -- nvim snippets with nvim-scissors
-vim.keymap.set('n', '<leader>se', function()
-    require('scissors').editSnippet()
-end)
+vim.keymap.set('n', '<leader>se',
+               function() require('scissors').editSnippet() end)
 -- When used in visual mode prefills the selection as body.
-vim.keymap.set({ 'n', 'x' }, '<leader>sa', function()
-    require('scissors').addNewSnippet()
-end)
+vim.keymap.set({'n', 'x'}, '<leader>sa',
+               function() require('scissors').addNewSnippet() end)
 
 -- LSP Servers
 
@@ -493,17 +403,40 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp
                                                                       .protocol
                                                                       .make_client_capabilities())
 
-require'lspconfig'.cssls.setup { capabilities = capabilities }
-require'lspconfig'.bashls.setup { capabilities = capabilities }
+require'lspconfig'.bashls.setup {capabilities = capabilities}
+
+require'lspconfig'.biome.setup {capabilities = capabilities}
+
 require'lspconfig'.lua_ls.setup {
     capabilities = capabilities,
-    settings = { Lua = { diagnostics = { globals = { 'vim' } } } }
+    settings = {Lua = {diagnostics = {globals = {'vim'}}}}
 }
-require'lspconfig'.tsserver.setup { capabilities = capabilities }
+
+require'lspconfig'.tailwindcss.setup {capabilities = capabilities}
+
+require'lspconfig'.tsserver.setup {
+    capabilities = capabilities,
+    init_options = {
+        plugins = {
+            {
+                name = "@vue/typescript-plugin",
+                location = home ..
+                    "/.local/share/nvim/mason/packages/vue-typescript-plugin/node_modules/@vue/typescript-plugin",
+                -- languages = {"javascript", "typescript", "vue"}
+                languages = {"vue"}
+            }
+        }
+    },
+    filetypes = {
+        'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue'
+    }
+}
+
 require'lspconfig'.basedpyright.setup {
     capabilities = capabilities,
-    settings = { basedpyright = { analysis = { typeCheckingMode = 'basic' } } }
+    settings = {basedpyright = {analysis = {typeCheckingMode = 'basic'}}}
 }
+
 require'lspconfig'.csharp_ls.setup {
     -- specify root_dir, so lsp can find all solutions related to your workspace
     capabilities = capabilities,
@@ -514,12 +447,17 @@ require'lspconfig'.csharp_ls.setup {
     end
 }
 
+require'lspconfig'.volar.setup {
+    capabilities = capabilities,
+    filetypes = {"vue"}
+}
+
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float)
 vim.keymap.set('n', '<F7>', vim.diagnostic.goto_prev)
 vim.keymap.set('n', '<F8>', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<F12>', ':FormatWrite<CR>')
+vim.keymap.set('n', '<F12>', ':Neoformat<CR>')
 
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
@@ -532,7 +470,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
         -- Buffer local mappings.
         -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local opts = { buffer = ev.buf }
+        local opts = {buffer = ev.buf}
 
         vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
         vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
@@ -548,6 +486,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
         end, opts)
         vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
         vim.keymap.set('n', 'gr', vim.lsp.buf.rename, opts)
-        vim.keymap.set({ 'n', 'v' }, '<leader>.', vim.lsp.buf.code_action, opts)
+        vim.keymap.set({'n', 'v'}, '<leader>.', vim.lsp.buf.code_action, opts)
     end
 })
